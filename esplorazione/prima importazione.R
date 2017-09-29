83b2ff64cba8a6a512f0dc401bc6f57d68de239b
Mint_rebecca_ufficio <olpantani@unifi.it>
2017-09-29 11:15:59 +0200

    Diviso in due il file corrispondenze, I nomi composti ancora generano problemi coi punti e i due punti. Prodotto pdf dotplot bwplot esplorativo

--- esplorazione/prima importazione.R
+++ esplorazione/prima importazione.R
@@ -26,8 +26,13 @@ df.data <-
                sep= ";", dec=",",
                skip=2, fileEncoding = "UTF-8")
 ## Corrispondenze tra massa e programmazione
-df.corrispondenze <-
-    read.table(file.path(DirData, "corrispondenze_nomi.csv" ),
+df.MOLTE <-
+    read.table(file.path(DirData, "corrispondenze_MOLTE.csv" ),
+               sep= ";", dec=",", header=TRUE,
+               fileEncoding = "UTF-8",
+               colClasses="character")
+df.composti <-
+    read.table(file.path(DirData, "corrispondenze_PLFA.csv" ),
                sep= ";", dec=",", header=TRUE,
                fileEncoding = "UTF-8",
                colClasses="character")
@@ -56,24 +61,22 @@ names(df.data)[c(1:2,4,7)] <-
       "DATA.FILE","INJ.DATE")
 
 
-
 df.data$INJ.DATE <-
     strptime(as.character(df.data$INJ.DATE), "%d/%m/%Y %R")
                                         #df.data <- df.data[-20,-(1:2)]
-
 
 require(plyr)
 df.data$STRINGA <-
     as.character(mapvalues(df.data$DATA.FILE,
                            from = as.character(unique(df.data$DATA.FILE)),
-                           to = df.corrispondenze$CAMPIONI.R
+                           to = df.MOLTE$CAMPIONI.R
                            )
                  )
-df.data <- df.data[-14,]
+                                        #df.data <- df.data[-14,] ## Per ora si toglie
 df.data$MAN <-
     factor(
         substr(df.data$STRINGA,1,2),
-        levels = c("CO", "OO")
+        levels = c("CO", "OO", "MS")
     )
 df.data$FIELD <-
     factor(
@@ -81,7 +84,8 @@ df.data$FIELD <-
     )
 df.data$TIL <-
     factor(
-        substr(df.data$STRINGA,5,5)
+        substr(df.data$STRINGA,5,5),
+        levels = c("A", "B", "C", "D")
     )
 df.data$TIL <-
     mapvalues(df.data$TIL,
@@ -93,9 +97,50 @@ df.data$PARC <-
         substr(df.data$STRINGA,6,6)
     )
 
-df.data <- df.data[,c(1:7,43:47, 8:42)]
+df.data <-
+    df.data[,c(1:7,43:47, 8:42)]
+
+names(df.data)[13:47]<- df.composti$NOME.R
 
 write.table(df.data, file=file.path(DirElab, "ottobre.csv"),
-               sep= ";", dec=",",
-               fileEncoding = "UTF-8",
-               col.names = NA)
+            sep= ";", dec=",",
+            fileEncoding = "UTF-8",
+            col.names = NA)
+
+#################################
+## Fine Importazione dati autunno
+#################################
+
+table(df.data$MAN, df.data$TIL)
+i.PLFA <- 13:47
+
+lis.data <- split(df.data, df.data$Type)
+require(lattice)
+
+raggruppa <-
+    with(lis.data[[2]], interaction(MAN,TIL))##,EXTRACT,INJECTION))
+
+pdf(file.path(DirGraf, "primo tentativo.pdf"))
+for (i in i.PLFA){
+    print(
+        dotplot(TIL ~ lis.data[[2]][,i],
+                data = lis.data[[2]],
+                groups = lis.data$Sample$MAN,
+                xlab = names(lis.data[[2]])[i],
+                key = simpleKey(levels(lis.data$Sample$MAN), space = "right"),
+                aspect = 0.8,
+                layout=c(1,1),
+                ylab=NULL, scale="free", pch=4, cex=2
+                )
+    )
+    print(
+        bwplot(raggruppa ~ lis.data[[2]][,i],
+               data = lis.data[[2]],
+               aspect = 0.8,
+               xlab = names(lis.data[[2]])[i],
+               ylab = NULL,
+               scale = "free", col=1:2,
+               )
+    )
+}
+dev.off()
