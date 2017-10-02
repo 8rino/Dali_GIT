76b621710f625f3e7ed8c08003f79249f406cdad
Mint_rebecca_ufficio <olpantani@unifi.it>
2017-10-02 11:34:51 +0200

    risolto problema importazione tra campioni cal e sample dovuti a un mancato sort dei nomi dentro a df.MOLTE.R. Rimane il problema della virgola e del punto e virgola

---
+++ esplorazione/prima esplorazione.R
@@ -0,0 +1,135 @@
+source("prima importazione.R")
+cbind.data.frame(df.data$MAN, df.data$Type)
+require(compositions)
+## lis.data$Sample <-
+##     lis.data$Sample[order(lis.data$Sample$INJ.DATE),]
+Y.aree <-
+    acomp(lis.data$Sample[,i.PLFA])
+fattore <- ##lis.data$Sample$TIL
+    interaction(lis.data$Sample$MAN,lis.data$Sample$TIL)
+lm.1 <-
+    lm(ilr(Y.aree) ~ fattore)
+anova(lm.1)
+
+qqnorm(ilrInv(resid(lm.1), orig=Y.aree))
+
+fun.biplot <-
+    function (x, y, var.axes = TRUE, cex = rep(par("cex"), 2),
+              xlabs = NULL, ylabs = NULL, expand = 1,
+              xlim  = NULL, ylim  = NULL, arrow.len = 0.1,
+              main = NULL, sub = NULL, xlab = NULL, ylab = NULL,
+              col, choices, scale, pc.biplot, col.obs, etich, ...)
+{
+    if (length(choices) != 2L)
+        stop("length of choices must be 2")
+    if (!length(scores <- x$scores))
+        stop(gettextf("object '%s' has no scores", deparse(substitute(x))),
+             domain = NA)
+    lam <- x$sdev[choices]
+    if (is.null(n <- x$n.obs))
+        n <- 1
+    lam <- lam * sqrt(n)
+    if (scale < 0 || scale > 1)
+        warning("'scale' is outside [0, 1]")
+    if (scale != 0)
+        lam <- lam^scale
+    else lam <- 1
+    if (pc.biplot)
+        lam <- lam/sqrt(n)
+    fun.MyBiplot(t(t(scores[, choices])/lam), t(t(x$loadings[,
+                                                             choices]) * lam), col.obs = col.obs, xlabs=etich, ...)
+    invisible()
+}
+fun.MyBiplot <-
+    function (x, y, var.axes = TRUE, col=2, cex = rep(par("cex"), 2),
+              xlabs = NULL, ylabs = NULL, expand = 1, xlim = NULL, ylim = NULL,
+              arrow.len = 0.1, main = NULL, sub = NULL, xlab = NULL, ylab = NULL,
+              col.obs, col, ...)
+{
+    n <- nrow(x)
+    p <- nrow(y)
+    if (missing(xlabs)) {
+        xlabs <- dimnames(x)[[1L]]
+        if (is.null(xlabs))
+            xlabs <- 1L:n
+    }
+    xlabs <- as.character(xlabs)
+    dimnames(x) <- list(xlabs, dimnames(x)[[2L]])
+    if (missing(ylabs)) {
+        ylabs <- dimnames(y)[[1L]]
+        if (is.null(ylabs))
+            ylabs <- paste("Var", 1L:p)
+    }
+    ylabs <- as.character(ylabs)
+    dimnames(y) <- list(ylabs, dimnames(y)[[2L]])
+    if (length(cex) == 1L)
+        cex <- c(cex, cex)
+    if (missing(col)) {
+        col <- par("col")
+        if (!is.numeric(col))
+            col <- match(col, palette(), nomatch = 1L)
+        col <- c(col, col + 1L)
+    }
+    else if (length(col) == 1L)
+        col <- c(col, col)
+    unsigned.range <- function(x) c(-abs(min(x, na.rm = TRUE)),
+                                    abs(max(x, na.rm = TRUE)))
+    rangx1 <- unsigned.range(x[, 1L])
+    rangx2 <- unsigned.range(x[, 2L])
+    rangy1 <- unsigned.range(y[, 1L])
+    rangy2 <- unsigned.range(y[, 2L])
+    if (missing(xlim) && missing(ylim))
+        xlim <- ylim <- rangx1 <- rangx2 <- range(rangx1, rangx2)
+    else if (missing(xlim))
+        xlim <- rangx1
+    else if (missing(ylim))
+        ylim <- rangx2
+    ratio <- max(rangy1/rangx1, rangy2/rangx2)/expand
+    on.exit(par(op))
+    op <- par(pty = "s")
+    if (!is.null(main))
+        op <- c(op, par(mar = par("mar") + c(0, 0, 1, 0)))
+    plot(x, type = "n", xlim = xlim, ylim = ylim, col = col[1L],
+         xlab = xlab, ylab = ylab, sub = sub, main = main, ...)
+    text(x, xlabs, cex = cex[1L], col = col.obs, ...)# col[1L], ...)
+    par(new = TRUE)
+    dev.hold()
+    on.exit(dev.flush(), add = TRUE)
+    plot(y, axes = FALSE, type = "n", xlim = xlim * ratio, ylim = ylim *
+                                                               ratio, xlab = "", ylab = "", col = col[1L], ...)
+    axis(3, col = col[2L], ...)
+    axis(4, col = col[2L], ...)
+    box(col = col[1L])
+    text(y, labels = ylabs, cex = cex[2L], col = col[2L], ...)
+    if (var.axes)
+        arrows(0, 0, y[, 1L] * 0.8, y[, 2L] * 0.8, col = col[2L],
+               length = arrow.len)
+    invisible()
+}
+
+x.PLFA <-
+    acomp(lis.data$Sample[,i.PLFA[-c(4,22,35)]])
+
+pcx.PLFA <- princomp(x.PLFA)
+
+##pdf(file.path(DirGraf, "PCAcomposizionale_Medie.pdf")
+colori <-    as.numeric(lis.data$Sample$MAN)+3
+    as.numeric(
+               interaction(lis.data$Sample$MAN,
+                           lis.data$Sample$TIL))
+fun.biplot(x=pcx.PLFA,  col=c(2, "transparent"),
+           ## main="foglie",
+           choices=c(1,2), ## assi/componenti da considerare
+           scale=1,
+           pc.biplot=TRUE,
+           col.obs = colori,
+           #etich = lis.data$Sample$STRINGA
+           )
+                                        # dev.off()
+
+plot(x=pcx.PLFA,
+            ## main="foglie",
+            choices=c(1,2), ## assi/componenti da considerare
+            scale=1,
+            pc.biplot=TRUE
+            )
--- esplorazione/prima importazione.R
+++ esplorazione/prima importazione.R
@@ -1,7 +1,10 @@
 info.sistema <-
     Sys.info()[c(1,7)]
 if(info.sistema[1]=="Windows"){
-    DirMain <- paste("C:/Users/", info.sistema[2], "/Dropbox/MOLTE_R", sep="")
+    DirMain <-
+        paste("C:/Users/",
+              info.sistema[2],
+              "/Dropbox/MOLTE_R", sep="")
 } else {
     if(info.sistema[2]=="dalila" ){
         DirMain <-"~/Dali_GIT"
@@ -20,11 +23,27 @@ DirFunz <-
     file.path(DirMain, "funzioni")
 DirCod <-
     file.path(DirMain, "codice")
+nome.file.da.importare <-
+    c("batch1ottobre.csv","batchsemicolon.csv")[1]
 ## Dati veri e propri
 df.data <-
-    read.table(file.path(DirData, "batch1ottobre.csv" ),
-               sep= ";", dec=",",
-               skip=2, fileEncoding = "UTF-8")
+    read.table(file.path(DirData, nome.file.da.importare ),
+               sep= ";",
+               skip=2)
+
+
+## df.data <-
+##     read.table(file.path(DirData, nome.file.da.importare ),
+##                sep= ",",
+##                skip=2)
+## df.bis$V7 <-
+##     strptime(as.character(df.bis$V7), "%d/%m/%Y %R")
+
+## df.bis <- df.bis[order(df.bis$V7),]
+
+## dim(df.data);dim(df.bis)
+
+
 ## Corrispondenze tra massa e programmazione
 df.MOLTE <-
     read.table(file.path(DirData, "corrispondenze_MOLTE.csv" ),
@@ -38,18 +57,19 @@ df.composti <-
                colClasses="character")
 ## Ginnastica per importare dati di massa
 vec.nomi.colonne <-
-    readLines(file.path(DirData, "batch1ottobre.csv" ),n=2)
+    readLines(file.path(DirData,
+                        nome.file.da.importare ),n=2)
 vec.nomi.colonne1 <-
     unlist(strsplit(vec.nomi.colonne[1], "[;]"))
+vec.nomi.colonne2 <-
+    unlist(strsplit(vec.nomi.colonne[2], "[;]"))
+## toglie la tringa finale "Results"
 vec.nomi.colonne1[-1] <-
     substr(vec.nomi.colonne1[-1],
            1,
            nchar(vec.nomi.colonne1[-1])-
            nchar("Results")-1
            )
-vec.nomi.colonne2 <-
-    unlist(strsplit(vec.nomi.colonne[2], "[;]"))
-
 vec.nomi <-
     c(vec.nomi.colonne2[1:7],
       vec.nomi.colonne1[-(1:7)]
@@ -59,24 +79,23 @@ names(df.data) <- vec.nomi
 names(df.data)[c(1:2,4,7)] <-
     c(paste("MH", 1:2, sep="."),
       "DATA.FILE","INJ.DATE")
+##df.data <- df.data[-20,-(1:2)]
 
-
-df.data$INJ.DATE <-
-    strptime(as.character(df.data$INJ.DATE), "%d/%m/%Y %R")
-                                        #df.data <- df.data[-20,-(1:2)]
-
+## Pericoloso modo di agire, ma necessario
+## Attenzione all'INDISPENSABILE sort !!!
 require(plyr)
 df.data$STRINGA <-
-    as.character(mapvalues(df.data$DATA.FILE,
-                           from = as.character(unique(df.data$DATA.FILE)),
-                           to = df.MOLTE$CAMPIONI.R
-                           )
-                 )
-                                        #df.data <- df.data[-14,] ## Per ora si toglie
+    as.character(
+        mapvalues(df.data$DATA.FILE,
+                  from =
+                      sort(as.character(unique(df.data$DATA.FILE))),
+                  to = sort(df.MOLTE$CAMPIONI.R)
+                  )
+    )
 df.data$MAN <-
     factor(
         substr(df.data$STRINGA,1,2),
-        levels = c("CO", "OO", "MS")
+        levels = c("CO", "OO", "MS", "ST")
     )
 df.data$FIELD <-
     factor(
@@ -97,10 +116,16 @@ df.data$PARC <-
         substr(df.data$STRINGA,6,6)
     )
 
-df.data <-
-    df.data[,c(1:7,43:47, 8:42)]
+## df.data$INJ.DATE <-
+##     strptime(as.character(df.data$INJ.DATE), "%d/%m/%Y %R")
+## df.data <- df.data[order(df.data$INJ.DATE),]
 
-names(df.data)[13:47]<- df.composti$NOME.R
+df.data <-
+    df.data[,c(1:7, 43:47, 8:42)]
+
+i.PLFA <- 13:47
+
+names(df.data)[i.PLFA] <- df.composti$NOME.R
 
 write.table(df.data, file=file.path(DirElab, "ottobre.csv"),
             sep= ";", dec=",",
@@ -112,21 +137,26 @@ write.table(df.data, file=file.path(DirElab, "ottobre.csv"),
 #################################
 
 table(df.data$MAN, df.data$TIL)
-i.PLFA <- 13:47
 
-lis.data <- split(df.data, df.data$Type)
+
+df.data$SOMMA.AREE <-
+    apply(df.data[,i.PLFA], 1,
+          function(x) sum(x, na.rm=TRUE))
+
+lis.data <-
+    split(df.data, df.data$Type)
 require(lattice)
 
 raggruppa <-
-    with(lis.data[[2]], interaction(MAN,TIL))##,EXTRACT,INJECTION))
+    with(lis.data$Sample, interaction(MAN,TIL))##,EXTRACT,INJECTION))
 
 pdf(file.path(DirGraf, "primo tentativo.pdf"))
-for (i in i.PLFA){
+for (i in c(i.PLFA, 48)){
     print(
-        dotplot(TIL ~ lis.data[[2]][,i],
-                data = lis.data[[2]],
+        dotplot(TIL ~ lis.data$Sample[,i],
+                data = lis.data$Sample,
                 groups = lis.data$Sample$MAN,
-                xlab = names(lis.data[[2]])[i],
+                xlab = names(lis.data$Sample)[i],
                 key = simpleKey(levels(lis.data$Sample$MAN), space = "right"),
                 aspect = 0.8,
                 layout=c(1,1),
@@ -134,10 +164,10 @@ for (i in i.PLFA){
                 )
     )
     print(
-        bwplot(raggruppa ~ lis.data[[2]][,i],
-               data = lis.data[[2]],
+        bwplot(raggruppa ~ lis.data$Sample[,i],
+               data = lis.data$Sample,
                aspect = 0.8,
-               xlab = names(lis.data[[2]])[i],
+               xlab = names(lis.data$Sample)[i],
                ylab = NULL,
                scale = "free", col=1:2,
                )
